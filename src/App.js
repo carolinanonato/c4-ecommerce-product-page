637204732c0adaf23fc5be228d57fc3032763d51
Carolina Nonato <csnonato@gmail.com>
2022-05-20 09:26:17 -0400

    product pictures

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Header from './components/Header';
+import ProductInfo from './components/ProductInfo';
+import ProductPictures from './components/ProductPictures';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div class="hero">
+        <ProductPictures />
+        <ProductInfo />
+
+      </div>
     </div>
   );
 }
---
+++ src/components/ProductPictures.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import imagemain from '../images/image-product-1.jpg'
+import thumb1 from '../images/image-product-1-thumbnail.jpg'
+import thumb2 from '../images/image-product-2-thumbnail.jpg'
+import thumb3 from '../images/image-product-3-thumbnail.jpg'
+import thumb4 from '../images/image-product-4-thumbnail.jpg'
+
+function ProductPictures() {
+    return (
+        <div className='product-images'>
+            <div className="main">
+                <img src={imagemain} alt="" />
+            </div>
+            <div className="thumbs">
+                <img src={thumb1} alt="" />
+                <img src={thumb2} alt="" />
+                <img src={thumb3} alt="" />
+                <img src={thumb4} alt="" />
+            </div>
+
+
+        </div>
+    )
+}
+
+export default ProductPictures
